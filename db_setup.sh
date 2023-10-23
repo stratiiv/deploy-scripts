66a28e166014b3abd10bc280c6beb65ace0ec3b5
stratiiv <stratiyvalter@gmail.com>
2023-10-23 17:57:35 +0300

    fix dumpfile path

--- db_setup.sh
+++ db_setup.sh
@@ -10,5 +10,8 @@ sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $PG_USER"
 sudo -u postgres bash -c "echo \"$DB_HOST:$DB_PORT:$DB_NAME:$PG_USER:$PG_PASSWORD\" > /var/lib/postgresql/.pgpass"
 chmod 0600 /var/lib/postgresql/.pgpass
 
+# copy dumpfile to postgres home
+cp "$DUMP_FILE" /var/lib/postgresql/
+
 # restore from dump
-sudo -u postgres psql -U "$PG_USER" -h "$DB_HOST" -d "$DB_NAME" -f "$DUMP_FILE"
+sudo -u postgres psql -U "$PG_USER" -h "$DB_HOST" -d "$DB_NAME" -f "/var/lib/postgresql/$DUMP_FILE"
