7b9a95636a482c20524b68097d02d5ed3f9ce450
stratiiv <stratiyvalter@gmail.com>
2023-10-23 17:32:27 +0300

    fix psql login bug

--- db_setup.sh
+++ db_setup.sh
@@ -7,7 +7,8 @@ fi
 
 sudo -u postgres psql -c "CREATE USER $PG_USER WITH PASSWORD '$PG_PASSWORD'"
 sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $PG_USER"
-sudo -u postgres echo "$DB_HOST:$DB_PORT:$DB_NAME:$PG_USER:$PG_PASSWORD" > "/var/lib/postgresql/.pgpass"
-chmod 600 "/var/lib/postgresql/.pgpass"
+sudo -u postgres bash -c "echo \"$DB_HOST:$DB_PORT:$DB_NAME:$PG_USER:$PG_PASSWORD\" > /var/lib/postgresql/.pgpass"
+chmod 0600 /var/lib/postgresql/.pgpass
+
 # restore from dump
-sudo -u postgres psql -U "$DB_USER" -h "$DB_HOST" -d "$DB_NAME" -f "$DUMP_FILE"
+sudo -u postgres psql -U "$PG_USER" -h "$DB_HOST" -d "$DB_NAME" -f "$DUMP_FILE"
