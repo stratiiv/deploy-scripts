4f35c745ae5fd766d0990af6dfd43fc814dc78a8
stratiiv <stratiyvalter@gmail.com>
2023-10-25 14:56:41 +0300

    refactor install script

--- install.sh
+++ install.sh
@@ -1,34 +1,26 @@
-sudo apt update -y
-# install required services
-sudo apt install -y gnupg curl
-sudo apt install -y git
-sudo apt install -y openjdk-11-jdk
-sudo apt install -y zip unzip
-# install postgres
+# update package lists and install required services
+apt update -y
+apt install -y gnupg curl git openjdk-11-jdk zip unzip
+
+# add the PostgreSQL repository and install PostgreSQL
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-sudo apt update -y
-sudo apt install -y postgresql-14 
-sudo apt install -y redis
-sudo apt install -y tomcat9
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - # change to gpg
+apt install -y postgresql-14
+
+# install Redis
+apt install -y redis
+
+# install Tomcat 9
+apt install -y tomcat9
+
+# install MongoDB
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
+   gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt update -y
-sudo apt-get install -y mongodb-org
-#install gradle
+apt update -y
+apt-get install -y mongodb-org
+
+# install Gradle using SDKMAN
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install gradle 7.3.3
-# running required services
-sudo systemctl daemon-reload
-sudo systemctl start mongod
-sudo systemctl start postgresql
-sudo systemctl start redis
-sudo systemctl start tomcat9
-# launch services on startup
-sudo systemctl enable mongod
-sudo systemctl enable postgresql
-sudo systemctl enable redis-server
-sudo systemctl enable tomcat9
