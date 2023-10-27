8d044ca76050e219c2a9cae0378c732611c0f821
stratiiv <stratiyvalter@gmail.com>
2023-10-27 14:51:58 +0300

    add nodejs install

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
-# add apt repositories for postgres and mongo
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+
+# add required repositories and gpg keys for apt
+# postgresql
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - # change to gpg
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /usr/share/keyrings/postgresql-archive-keyring.gpg
+echo 'deb [signed-by=/usr/share/keyrings/postgresql-archive-keyring.gpg] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+# mongodb
 curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-   gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+# nodejs & npm
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
 
 # update package lists and install required services
-apt update -y
-apt install -y gnupg curl git openjdk-11-jdk zip unzip redis tomcat9 postgresql-14 mongodb-org
+apt-get update -y
+apt-get install -y git openjdk-11-jdk zip unzip redis tomcat9 postgresql-14 mongodb-org nodejs
 
 # install Gradle using SDKMAN
 curl -s "https://get.sdkman.io" | bash
